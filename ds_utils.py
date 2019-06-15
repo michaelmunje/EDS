b09b666f792c2d637a34cb00533fc0ce860317f7
Michael Munje <michaelmunje@gmail.com>
2019-06-14 23:16:54 -0700

    Add function to survey different regressors.

--- ds_utils.py
+++ ds_utils.py
@@ -9,9 +9,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import HuberRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn import svm
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import accuracy_score
@@ -195,8 +205,7 @@ def replace_missing_with_ml(df: pd.DataFrame, col_to_predict: str) -> pd.DataFra
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42)
 
     if df[col_to_predict].dtype.name == 'category':
-        rf = GradientBoostingClassifier(
-                                        learning_rate=0.05, max_features='sqrt',
+        rf = GradientBoostingClassifier(learning_rate=0.05, max_features='sqrt',
                                         min_impurity_split=None, min_samples_leaf=15,
                                         min_samples_split=10, n_estimators=12000)
     else:
@@ -283,15 +292,17 @@ def evaluate_regressor(y_actual: np.array, y_pred: np.array, metric_func: functi
     return metrics
 
 
-def cross_validate(model, x, y, metric, folds=5, repeats=3):
+def cross_validate(model, x: np.array, y: np.array, metric: function, folds: int = 5,
+                   repeats: int = 3, verbose: bool = True) -> float:
     """
-    Function to do the cross validation - using stacked Out of Bag method instead of averaging across folds.
+    Perform k-fold cross-validation using the out of the bag score.
     :param model: scikit-learn like model to perform cross validation on
     :param x: numpy array of features
     :param y: numpy array of predictors
     :param metric: what metric to use to evaluate the models
-    :param folds: number of folds. Default = 10.
-    :param repeats: number of times to repeat the whole process (different random splitting). Default = 5.
+    :param folds: number of folds. Default = 5.
+    :param repeats: number of times to repeat the whole process (different random splitting). Default = 3.
+    :param verbose: Whether to print progress messages. Default = True.
     :return: the average metric score across all folds and repeats
     """
 
@@ -299,24 +310,70 @@ def cross_validate(model, x, y, metric, folds=5, repeats=3):
     score = np.zeros(repeats)
 
     for r in range(repeats):
-
-        print('Running k-fold cross-validation ', r + 1, '/', repeats)
-
+        if verbose:
+            print('Running k-fold cross-validation ', r + 1, '/', repeats)
         x, y = shuffle(x, y, random_state=r)
 
         for i, (train_ind, test_ind) in enumerate(KFold(n_splits=folds, random_state=r + 10).split(x)):
-
-            print('Computing fold ', i + 1, '/', folds)
-
+            if verbose:
+                print('Computing fold ', i + 1, '/', folds)
             x_train, y_train = x[train_ind, :], y[train_ind]
             x_test, y_test = x[test_ind, :], y[test_ind]
-
             model.fit(x_train, y_train)
-
             y_pred[test_ind] = model.predict(x_test)
-
         score[r] = metric(y_pred, y)
 
-    print('\nOverall ', metric.__name__, ': ', score)
-    print('Average:', np.mean(score))
-    print('Std. Dev:', np.std(score))
+    print('Evaluation metric:', metric.__name__)
+    print('Average:', np.round(np.mean(score), 4))
+    print('Std. Dev:', np.round(np.std(score), 4))
+    mean = np.mean(score)
+    return mean[0] if len(mean) > 1 else mean
+
+
+class Ensemble:
+    def __init__(self, model):
+        self.model = model
+
+
+def try_many_regressors(x: np.array, y: np.array, metric: function, metric_max_better: bool = True) -> None:
+    """
+    Tries a few solid regressors in sklearn and returns the best performing one
+    :param x: numpy array of the features
+    :param y: numpy array of the predictor
+    :param metric: Function, the evaluation metric to use.
+    :param metric_max_better: If the metric's higher value means better value
+    """
+
+    gb = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10,
+                                   loss='huber', random_state=42)
+
+    gb2 = GradientBoostingRegressor(learning_rate=0.05, max_features='sqrt', loss='huber',
+                                    min_impurity_split=None, min_samples_leaf=15,
+                                    min_samples_split=10, n_estimators=12000,
+                                    random_state=42)
+
+    lasso = make_pipeline(RobustScaler(), Lasso(alpha=0.0005, random_state=42))
+    elastic = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, max_iter=10000, random_state=42))
+    rf = RandomForestRegressor(n_estimators=200, min_samples_leaf=3, random_state=42)
+    rrf = ExtraTreesRegressor(n_estimators=200, min_samples_leaf=3, random_state=42)
+    huber = HuberRegressor()
+    linear = LinearRegression()
+    nn = MLPRegressor(hidden_layer_sizes=(1000, 10), learning_rate='adaptive',
+                      max_iter=1000, random_state=42, early_stopping=True)
+    svm_r = svm.SVR(kernel='poly', gamma='auto')
+
+    regressors = [gb, gb2, lasso, elastic, rf, rrf, huber, linear, nn, svm_r]
+    scores = np.zeros(len(regressors))
+
+    for i, r in enumerate(regressors):
+        print('Running k-fold cross validation for', r.__class__.__name__)
+        scores[i] = cross_validate(r, x, y, metric)
+
+    best_index = np.argmax if metric_max_better else np.argmin
+    best = np.amax if metric_max_better else np.amin
+    first = lambda x: x[0] if len(x) > 1 else x
+
+    print('Best performing model: ', regressors[first(best_index(scores))].__class__.__name__)
+    print('Best', metric.__name__, ':', best(scores))
