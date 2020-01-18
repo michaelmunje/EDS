52e15d4291673c3a928a20ccc98e4ba2bfb94539
Michael Munje <michaelmunje@gmail.com>
2020-01-18 15:23:10 -0800

    Linting cleanup, fix imports.

--- ds_utils/ensemble.py
+++ ds_utils/ensemble.py
@@ -1,9 +1,9 @@
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
 from scipy.optimize import differential_evolution
-from sklearn.metrics import mean_squared_error
 import metrics
 import _default_models
 from typing import Callable
+import numpy as np
 from numpy.linalg import norm
 from abc import ABC, abstractmethod
 
@@ -15,11 +15,11 @@ class Ensemble(ABC):
         self.models = models
         self.weights = [1/len(self.models)] * len(self.models) if not weights else weights
         self.names = names
-        
+
     @abstractmethod
     def predict(self, x: np.array) -> np.array:
         pass
-    
+
     @abstractmethod
     def _get_model_pred(self, model_index: int, X: np.array):
         pass
@@ -41,23 +41,21 @@ class Ensemble(ABC):
     def __normalize_weights(self):
         result = norm(self.weights, 1)
         self.weights = self.weights / result if not result == 0.0 else self.weights
-    
+
     def optimize_weights(self, X: np.array, Y: np.array, iterations: int = 1000) -> None:
         model_preds, y_holdout_true = self.__get_cv_holdout_results(X, Y)
-        
-        weights = [1.0/len(self.models)] * len(self.models)
-        
+
         bounds = [(0.0, 1.0)] * len(self.models)
-        result = differential_evolution(self.__evaluate_ensemble_weights, bounds, 
+        result = differential_evolution(self.__evaluate_ensemble_weights, bounds,
                                         args=(model_preds, y_holdout_true),
                                         maxiter=iterations, tol=1e-7)
         self.weights = result['x']
         self.__normalize_weights()
-        
+
     def get_model_importances(self):
         if self.names:
             tuples = list(zip(self.names, [round(w, 3) for w in self.weights]))
-            return sorted(tuples, key = lambda x: x[1], reverse=True)
+            return sorted(tuples, key=lambda x: x[1], reverse=True)
         return sorted(self.weights, reverse=True)
 
     def __evaluate_ensemble_weights(self, weights: np.array, model_preds: np.array, y_test: np.array) -> float:
@@ -78,11 +76,13 @@ class Ensemble(ABC):
 
 
 class EnsembleClassifier(Ensemble):
-        
-    def __init__(self, models, names: [str] = None, weights: np.array = None, metric: Callable[[np.array, np.array], float] = roc_auc_error):
+
+    def __init__(self, models, names: [str] = None, weights: np.array = None, 
+                 metric: Callable[[np.array, np.array], float] = metrics.roc_auc_error):
+
         self.metric = metric
         super().__init__(models, names, weights)
-        
+
     def predict(self, x: np.array, prob=False) -> np.array:
         model_preds = [model.predict(x) if not prob else model.predict_proba(x) for model in self.models]
         preds = sum(x * y for x, y in zip(model_preds, self.weights))
@@ -90,34 +90,39 @@ class EnsembleClassifier(Ensemble):
 
     def predict_proba(self, x: np.array) -> np.array:
         return self.predict(x, prob=True)
-    
-    def _get_model_pred(self, model_index: int, x: np.array) -> np.array:
-        return self.models[model_index].predict_proba(x)[:,1]
 
+    def _get_model_pred(self, model_index: int, x: np.array) -> np.array:
+        return self.models[model_index].predict_proba(x)[:, 1]
+
+    @staticmethod
     def get_default_models() -> []:
         return _default_models.get_default_classfiers()
 
+    @staticmethod
     def get_default_models_names() -> [str]:
         return _default_models.get_default_classfiers_names()
 
-    
 
 class EnsembleRegressor(Ensemble):
-        
-    def __init__(self, models, names: [str] = None, weights: np.array = None, metric: Callable[[np.array, np.array], float] = mean_squared_error):
+
+    def __init__(self, models, names: [str] = None, weights: np.array = None,
+                 metric: Callable[[np.array, np.array], float] = metrics.mse):
+
         self.metric = metric
         super().__init__(models, names, weights)
-        
+
     def predict(self, x: np.array) -> np.array:
         model_preds = [model.predict(x) for model in self.models]
         preds = sum(x * y for x, y in zip(model_preds, self.weights))
         return np.array([1 if pred >= 0.5 else 0 for pred in preds])
-    
+
     def _get_model_pred(self, model_index: int, X: np.array) -> np.array:
         return self.models[model_index].predict(X)
 
+    @staticmethod
     def get_default_models() -> []:
         return _default_models.get_default_regressors()
 
+    @staticmethod
     def get_default_models_names() -> [str]:
         return _default_models.get_default_regressors_names()
--- ds_utils/metrics.py
+++ ds_utils/metrics.py
@@ -2,17 +2,26 @@ import numpy as np
 from sklearn.metrics import recall_score
 from sklearn.metrics import auc
 from sklearn.metrics import roc_curve
+from sklearn.metrics import mean_squared_error
+
 
 def false_positive_rate(Y: np.array, Y_hat: np.array) -> np.array:
-    return 1 - recall_score(Y,Y_hat)
+    return 1 - recall_score(Y, Y_hat)
+
 
 def false_negative_rate(Y: np.array, Y_hat: np.array) -> np.array:
     return 1 - recall_score(1-Y, 1-Y_hat)
+
 
 def avg_error(Y: np.array, Y_hat: np.array) -> np.array:
     Y_hat = np.array([1 if pred >= 0.5 else 0 for pred in Y_hat])
     return (false_positive_rate(Y_hat, Y) + false_negative_rate(Y_hat, Y)) / 2
 
+
 def roc_auc_error(Y, Y_hat):
     fpr, tpr, thresholds = roc_curve(Y, Y_hat)
     return 1 - auc(fpr, tpr)
+
+
+def mse(Y, Y_hat):
+    return mean_squared_error(Y, Y_hat)
