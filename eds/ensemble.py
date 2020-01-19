0c928116e1589127f935dd990848c5d75f4a969f
Michael Munje <michaelmunje@gmail.com>
2020-01-18 22:01:10 -0800

    Add docstrings for ensemble.

--- eds/ensemble.py
+++ eds/ensemble.py
@@ -9,12 +9,18 @@ from abc import ABC, abstractmethod
 
 
 class Ensemble(ABC):
+    """
+    Base class for an sklearn model-like wrapper for an ensemble of models.
+    """
+    @abstractmethod
+    def __init__(self, models: [] = None, names: [str] = None, weights: np.array = None):
+        self.models = models if models else Ensemble.get_default_models()
+        self.weights = weights if weights else [1/len(self.models)] * len(self.models)
+        self.names = names if models else Ensemble.get_default_models_names()
 
     @abstractmethod
-    def __init__(self, models, names: [str] = None, weights: np.array = None):
-        self.models = models
-        self.weights = [1/len(self.models)] * len(self.models) if not weights else weights
-        self.names = names
+    def predict(self, x: np.array) -> np.array:
+        pass
 
     @abstractmethod
     def _get_model_pred(self, model_index: int, X: np.array):
@@ -22,16 +28,12 @@ class Ensemble(ABC):
 
     @staticmethod
     @abstractmethod
-    def get_default_models():
+    def get_default_models() -> []:
         pass
 
     @staticmethod
     @abstractmethod
-    def get_default_models_names():
-        pass
-
-    @abstractmethod
-    def predict(self, x: np.array) -> np.array:
+    def get_default_models_names() -> [str]:
         pass
 
     def fit(self, X: np.array, Y: np.array) -> None:
@@ -79,29 +81,106 @@ class EnsembleClassifier(Ensemble):
 
     def __init__(self, models, names: [str] = None, weights: np.array = None,
                  metric: Callable[[np.array, np.array], float] = metrics.roc_auc_error):
+        """
+        Class for ensemble of classifiers which is an sklearn-like model but for an ensemble of classifiers.
 
+        Parameters
+        ----------
+        models : [], optional
+            List (or sequence-like object) containing predefined sklearn-like classifiers
+            If no models provided, uses a default list of models.
+
+        names : [type], optional
+            List of strings containing the names of the models. Defaults to None.
+            If no models provided, uses a default list of model names.
+
+        weights : np.array, optional
+            Predefined set of weights.
+            Defaults to equally weighted ensemble if no argument provided.
+
+        metric : Callable[[np.array, np.array], float], optional
+            Function for computing a metric to minimize.
+            A wrapper function can multiply a function to maximize by -1 to minimize instead.
+            Defaults to minimize the error of ROC Area under the curve
+        """
         self.metric = metric
         super().__init__(models, names, weights)
 
     def predict(self, x: np.array) -> np.array:
+        """
+        Predicts classes of feature vectors with the ensemble.
+        Note: Binary output is only supported at this time.
+
+        Parameters
+        ----------
+        x : np.array
+            Set of feature vectors to predict classes for.
+
+        Returns
+        -------
+        np.array
+            Ensemble's prediction of binary classes.
+        """
         model_preds = [model.predict(x) for model in self.models]
         preds = sum(x * y for x, y in zip(model_preds, self.weights))
         return np.array([1 if pred >= 0.5 else 0 for pred in preds])
 
     def predict_proba(self, x: np.array) -> np.array:
+        """
+        Predicts the class probability vector for the feature vectors with the ensemble.
+
+        Parameters
+        ----------
+        x : np.array
+            Set of feature vectors.
+
+        Returns
+        -------
+        np.array
+            Prediction for the classes probabilities for each feature vector.
+        """
         model_preds = [model.predict_proba(x) for model in self.models]
         preds = sum(x * y for x, y in zip(model_preds, self.weights))
         return np.array(preds)
 
     def _get_model_pred(self, model_index: int, x: np.array) -> np.array:
+        """
+        Gets the prediction of a fecture vector of a specific model in the ensemble.
+
+        Parameters
+        ----------
+        model_index : int
+            Index in ensemble to get classifier prediction for.
+
+        X : np.array
+            Input array consisting of feature vector to predict output for.
+        """
         return self.models[model_index].predict_proba(x)[:, 1]
 
     @staticmethod
     def get_default_models() -> []:
+        """
+        Gets an array consisting of predefined classifiers.
+        The specific classifiers can be found in :ref:`User Guide <ensemble>`
+
+        Returns
+        -------
+        []
+            An array consisting of predefined classifiers.
+        """
         return _default_models.get_default_classfiers()
 
     @staticmethod
     def get_default_models_names() -> [str]:
+        """
+        Gets an array consisting of predefined classifiers' names.
+        The specific classifiers can be found in :ref:`User Guide <ensemble>`
+
+        Returns
+        -------
+        []
+            An array consisting of string names of predefined classifiers.
+        """
         return _default_models.get_default_classfiers_names()
 
 
@@ -109,22 +188,81 @@ class EnsembleRegressor(Ensemble):
 
     def __init__(self, models, names: [str] = None, weights: np.array = None,
                  metric: Callable[[np.array, np.array], float] = metrics.mse):
+        """
+        Class for ensemble of regressors which is an sklearn-like model but for an ensemble of regressors.
 
+        Parameters
+        ----------
+        models : [], optional
+            List (or sequence-like object) containing predefined sklearn-like regressors
+            If no models provided, uses a default list of models.
+
+        names : [type], optional
+            List of strings containing the names of the models. Defaults to None.
+            If no models provided, uses a default list of model names.
+
+        weights : np.array, optional
+            Predefined set of weights.
+            Defaults to equally weighted ensemble if no argument provided.
+
+        """
         self.metric = metric
         super().__init__(models, names, weights)
 
     def predict(self, x: np.array) -> np.array:
+        """
+        Predicts output scalar of feature vectors with the ensemble.
+
+        Parameters
+        ----------
+        x : np.array
+            Set of feature vectors to predict output scalar for.
+
+        Returns
+        -------
+        np.array
+            Ensemble's regression prediction for each feature vector.
+        """
         model_preds = [model.predict(x) for model in self.models]
         preds = sum(x * y for x, y in zip(model_preds, self.weights))
         return np.array(preds)
 
     def _get_model_pred(self, model_index: int, X: np.array) -> np.array:
+        """
+        Gets the prediction array for a specific model in the ensemble.
+
+        Parameters
+        ----------
+        model_index : int
+            Index in ensemble to get model prediction for.
+
+        X : np.array
+            Input array consisting of feature vector to predict output for.
+        """
         return self.models[model_index].predict(X)
 
     @staticmethod
     def get_default_models() -> []:
+        """
+        Gets an array consisting of predefined regressors.
+        The specific regressors can be found in :ref:`User Guide <ensemble>`
+
+        Returns
+        -------
+        []
+            An array consisting of predefined regressors.
+        """
         return _default_models.get_default_regressors()
 
     @staticmethod
     def get_default_models_names() -> [str]:
+        """
+        Gets an array consisting of predefined regressors' names.
+        The specific regressors can be found in :ref:`User Guide <ensemble>`
+
+        Returns
+        -------
+        []
+            An array consisting of string names of predefined regressors.
+        """
         return _default_models.get_default_regressors_names()
