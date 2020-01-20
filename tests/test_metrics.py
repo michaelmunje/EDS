70cfe4485d4fb69df8c0eed2e45b7398d3047d3f
Michael Munje <michaelmunje@gmail.com>
2020-01-20 02:02:42 -0800

    Add more tests for metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -9,3 +9,35 @@ def test_fp_rate():
     y_pred = [1, 1]
     fp_rate = metrics.false_positive_rate(np.array(y_true), np.array(y_pred))
     assert(fp_rate == 0.5)
+
+    y_true = [1, 0, 1, 1]
+    y_pred = [1, 1, 1, 1]
+    fp_rate = metrics.false_positive_rate(np.array(y_true), np.array(y_pred))
+    assert(fp_rate == 0.25)
+
+    y_true = [1, 1, 1, 1]
+    y_pred = [1, 1, 1, 1]
+    fp_rate = metrics.false_positive_rate(np.array(y_true), np.array(y_pred))
+    assert(fp_rate == 0.0)
+
+
+def test_fn_rate():
+    y_true = [1, 0]
+    y_pred = [1, 1]
+    fn_rate = metrics.false_negative_rate(np.array(y_true), np.array(y_pred))
+    assert(fn_rate == 1.0)
+
+    y_true = [1, 0, 1, 1]
+    y_pred = [1, 1, 0, 1]
+    fn_rate = metrics.false_negative_rate(np.array(y_true), np.array(y_pred))
+    assert(fn_rate == 1.0)
+
+    y_true = [0, 0, 1, 0]
+    y_pred = [0, 0, 0, 0]
+    fn_rate = metrics.false_negative_rate(np.array(y_true), np.array(y_pred))
+    assert(fn_rate == 0.25)
+
+    y_true = [1, 1, 1, 1]
+    y_pred = [1, 1, 1, 1]
+    fn_rate = metrics.false_positive_rate(np.array(y_true), np.array(y_pred))
+    assert(fn_rate == 0.0)
